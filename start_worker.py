bae4e50cbcdefc89ee8b3e12d6e7b96dc2a72157
Noah Solomon <noahsolomon0518@gmail.com>
2022-02-17 14:38:02 -0500

    start worker module

---
+++ start_worker.py
@@ -0,0 +1,21 @@
+import os
+from redis import Redis
+from rq import Queue, Connection, Worker
+
+redis_host = "redis-15044.c278.us-east-1-4.ec2.cloud.redislabs.com"
+redis_port = 15044
+redis_password = "sPSPqmznaof65h2ltypLo9Bn1U29oLs1"
+
+
+
+redis_conn = Redis(redis_host, redis_port, password = redis_password)
+generate_playlist_queue = Queue(connection=redis_conn) 
+
+
+
+
+
+if __name__ == '__main__':
+    with Connection(redis_conn):
+        worker = Worker(generate_playlist_queue)
+        worker.work()
